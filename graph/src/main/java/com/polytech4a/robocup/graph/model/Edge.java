98d7cd6b779dd8ed1142cdc6434ce94124a8c6be
Dimitri <d.rodarie@gmail.com>
2015-05-06 16:16:12 +0200

    DRE : Complete Edge class

--- graph/src/main/java/com/polytech4a/robocup/graph/model/Edge.java
+++ graph/src/main/java/com/polytech4a/robocup/graph/model/Edge.java
@@ -1,5 +1,8 @@
 package com.polytech4a.robocup.graph.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Antoine CARON on 06/05/2015.
  *
@@ -7,4 +10,59 @@ package com.polytech4a.robocup.graph.model;
  * @version 1.0
  */
 public class Edge {
+    private Map<String, String> parameters;
+
+    /**
+     * Constructor of the Edge
+     * @param n1 id of the first node
+     * @param n2 id of the second node
+     */
+    public Edge (int n1, int n2){
+        this.parameters = new HashMap<String, String>();
+        this.parameters.put("nd1",String.valueOf(n1));
+        this.parameters.put("nd2",String.valueOf(n2));
+    }
+
+    /**
+     * Getter of the first node
+     * @return
+     */
+    public int getNode1() {
+        return Integer.valueOf(parameters.get("nd1"));
+    }
+
+    /**
+     * Getter of the second node
+     * @return
+     */
+    public int getNode2() {
+        return Integer.valueOf(parameters.get("nd2"));
+    }
+
+    /**
+     * Getter of the Edge parameters
+     * @return Map of parameters
+     */
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public boolean equals(Edge obj) {
+            return obj.getNode1() == this.getNode1() && obj.getNode2() == this.getNode2();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("Edge [").append(this.getNode1()).append(";")
+                .append(this.getNode2()).append("] : Parameters [");
+        for (String key: parameters.keySet()){
+            str.append("[").append(key).append(";").append(parameters.get(key)).append("]");
+        }
+        return str.append("]").toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Edge(this.getNode1(), this.getNode2());
+    }
 }
