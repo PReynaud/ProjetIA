35a472ee1ac18d748bdcbb38e92d5f000d6fe3e3
Dimitri <d.rodarie@gmail.com>
2015-05-06 17:16:22 +0200

    Add and delete functions for the graph class

--- graph/src/main/java/com/polytech4a/robocup/graph/model/Graph.java
+++ graph/src/main/java/com/polytech4a/robocup/graph/model/Graph.java
@@ -1,6 +1,7 @@
 package com.polytech4a.robocup.graph.model;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Antoine CARON on 06/05/2015.
@@ -22,29 +23,75 @@ public class Graph {
      */
     private ArrayList<Edge> edges;
 
+    /**
+     * Constructor for the Graph class
+     */
     public Graph() {
         this.edges=new ArrayList<Edge>();
         this.nodes=new ArrayList<Node>();
     }
 
+    /**
+     * Add node to the graph if the node doesn't exists
+     * @param node Node to add
+     */
     public void addNode(Node node){
-        nodes.add(node);
+        try{
+            getNode(node.getId());
+        }catch (NoSuchElementException e){
+            nodes.add(node);
+        }
     }
 
+    /**
+     * Remove node from the graph
+     * @param node Node to remove
+     */
     public void removeNode(Node node){
         nodes.remove(node);
     }
 
+    /**
+     * Add edge to the graph if the edge doesn't exist yet
+     * @param edge Edge to add
+     */
     public void addEdge(Edge edge){
-        edges.add(edge);
+        try{
+            getEdge(getNode(edge.getNode1()), getNode(edge.getNode2()));
+        }catch (NoSuchElementException e){
+            edges.add(edge);
+        }
     }
 
+    /**
+     * Add edge to the graph between the two nodes if the
+     * edge doesn't exist yet
+     * @param node1 First node of the edge
+     * @param node2 Second node of the edge
+     */
+    public void addEdge(Node node1, Node node2){
+        try{
+            getEdge(node1,node2);
+        }catch (NoSuchElementException e){
+            edges.add(new Edge(node1.getId(),node2.getId()));
+        }
+    }
+
+    /**
+     * Remove edge from the graph
+     * @param edge Edge to remove
+     */
     public void removeEdge (Edge edge){
         edges.remove(edge);
     }
 
+    /**
+     * Get the idNode node of the graph
+     * @param idNode id of the Node
+     * @return idNode Node
+     */
     public Node getNode (int idNode){
-        return nodes.get(idNode);
+        return nodes.parallelStream().filter(n->n.getId()==idNode).findAny().get();
     }
 
     public Edge getEdge (Node node1, Node node2){
@@ -72,6 +119,4 @@ public class Graph {
         }
         return clonedEdges;
     }
-
-
 }
