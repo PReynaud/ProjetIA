9f0342ae7bc8c41e7d8983ed1b069fb0ec32bed4
Slashgear <antoine395.caron@laposte.net>
2015-05-06 16:20:30 +0200

    ACN: Add Graph and Node structure.

--- graph/src/main/java/com/polytech4a/robocup/graph/model/Graph.java
+++ graph/src/main/java/com/polytech4a/robocup/graph/model/Graph.java
@@ -1,10 +1,37 @@
 package com.polytech4a.robocup.graph.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Antoine CARON on 06/05/2015.
  *
  * @author Antoine CARON
  * @version 1.0
+ *
+ * Simple Graph Model.
  */
 public class Graph {
+
+    /**
+     * Nodes of the graph.
+     */
+    private ArrayList<Node> nodes;
+
+    /**
+     * Edge of the graph.
+     */
+    private ArrayList<Edge> edges;
+
+    public Graph() {
+        this.edges=new ArrayList<Edge>();
+        this.nodes=new ArrayList<Node>();
+    }
+
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+
+    public void removeNode(Node node){
+        nodes.remove(node);
+    }
 }
--- graph/src/main/java/com/polytech4a/robocup/graph/model/Node.java
+++ graph/src/main/java/com/polytech4a/robocup/graph/model/Node.java
@@ -1,10 +1,52 @@
 package com.polytech4a.robocup.graph.model;
 
+import java.util.HashMap;
+
 /**
  * Created by Antoine CARON on 06/05/2015.
  *
  * @author Antoine CARON
  * @version 1.0
+ *
+ * Node object of a Graph.
  */
 public class Node {
+
+    /**
+     * Parameters of a Node.
+     */
+    private HashMap<String,String> parameters;
+
+    public HashMap<String, String> getParameters() {
+        return parameters;
+    }
+
+    public int getId(){
+        return Integer.valueOf(parameters.get("id")).intValue();
+    }
+
+    public Node(int id) {
+        this.parameters =new HashMap<String, String>();
+        parameters.put("id", String.valueOf(id));
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer stbf=new StringBuffer();
+        stbf.append("Node{");
+        for(String key:parameters.keySet()){
+            stbf.append(key).append("=").append(parameters.get(key)).append(",");
+        }
+        stbf.append("}");
+        return stbf.toString();
+    }
+
+    @Override
+    protected Node clone() throws CloneNotSupportedException {
+        return new Node(Integer.valueOf(parameters.get("id")).intValue());
+    }
+
+    public boolean equals(Node node) {
+        return this.getId()==node.getId();
+    }
 }
