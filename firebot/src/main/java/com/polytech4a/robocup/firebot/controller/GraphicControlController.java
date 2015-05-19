758799f3d64728c07094be1cd4936670e860a8e0
PReynaud <pierre.reynaud@hotmail.fr>
2015-05-19 23:19:07 +0200

    PRD: Addition of the edges to the view

--- firebot/src/main/java/com/polytech4a/robocup/firebot/controller/GraphicControlController.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/controller/GraphicControlController.java
@@ -8,6 +8,9 @@ import java.awt.event.ActionEvent;
 
 /**
  * Created by Pierre on 11/05/2015.
+ * @version 1.0
+ *          <p/>
+ *          Control class for the buttons that manipulate the graphic interface
  */
 public class GraphicControlController {
     private MainController mainController;
@@ -17,12 +20,15 @@ public class GraphicControlController {
 
         GraphicControlPanel graphicControlPanel = (GraphicControlPanel) mainController.getView().getGraphicControlPanel();
         graphicControlPanel.getAddNodeButton().addAction(new AddNodeAction(mainController));
-        graphicControlPanel.getAddEdgeButton().addAction(new AddEdgeAction());
+        graphicControlPanel.getAddEdgeButton().addAction(new AddEdgeAction(mainController));
         graphicControlPanel.getAddRobotButton().addAction(new AddRobotAction());
         graphicControlPanel.getAddFireButton().addAction(new AddFireAction());
     }
 }
 
+/**
+ * Action for the button that add a node. We change the selection mode in the main controller.
+ */
 class AddNodeAction extends AbstractAction{
     private MainController mainController;
 
@@ -36,15 +42,25 @@ class AddNodeAction extends AbstractAction{
     }
 }
 
+/**
+ * Action for the class that add an edge. We change the selection mode in the main controller.
+ */
 class AddEdgeAction extends AbstractAction{
-    public AddEdgeAction(){}
+    private MainController mainController;
+
+    public AddEdgeAction(MainController mainController){
+        this.mainController = mainController;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        mainController.setSelectionMode(EnumSelection.ADD_EDGE);
     }
 }
 
+/**
+ * Action for the class that add a robot. We change the selection mode in the main controller.
+ */
 class AddRobotAction extends AbstractAction{
     public AddRobotAction(){}
 
@@ -54,6 +70,9 @@ class AddRobotAction extends AbstractAction{
     }
 }
 
+/**
+ * Action for the class that add a fire. We change the selection mode in the main controller.
+ */
 class AddFireAction extends AbstractAction{
     public AddFireAction(){}
 
--- firebot/src/main/java/com/polytech4a/robocup/firebot/ui/graphic/models/Node.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/ui/graphic/models/Node.java
@@ -4,13 +4,22 @@ import java.awt.*;
 
 /**
  * Created by Pierre on 16/05/2015.
+ * @version 1.0
+ *          <p/>
+ *          Class for displaying the node in the graphic panel
  */
 public class Node {
     private int x;
     private int y;
 
+    /**
+     * Radius of the point which will be drawn
+     */
     private int size;
 
+    /**
+     * Color of the point
+     */
     protected Color color;
 
     public Node(int x, int y){
@@ -21,12 +30,6 @@ public class Node {
         this.color = Color.MAGENTA;
     }
 
-
-    public void drawNode(Graphics g){
-        g.setColor(this.color);
-        g.fillOval(x-(size/2), y-(size/2), size, size);
-    }
-
     public int getX() {
         return x;
     }
@@ -34,4 +37,18 @@ public class Node {
     public int getY() {
         return y;
     }
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Draw the node
+     * @param g Graphic object from the graphic panel
+     */
+    public void drawNode(Graphics g){
+        g.setColor(this.color);
+        g.fillOval(x-(size/2), y-(size/2), size, size);
+    }
+
 }
