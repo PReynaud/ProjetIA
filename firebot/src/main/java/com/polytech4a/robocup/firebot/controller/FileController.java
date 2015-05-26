1788ac37dfccdb9396e4257682245fb513e31347
PReynaud <pierre.reynaud@hotmail.fr>
2015-05-26 17:57:14 +0200

    PRD: displaying of an image, buffered image for the canvas

--- firebot/src/main/java/com/polytech4a/robocup/firebot/controller/FileController.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/controller/FileController.java
@@ -1,10 +1,15 @@
 package com.polytech4a.robocup.firebot.controller;
 
 import com.polytech4a.robocup.firebot.ui.FilePanel;
+import com.polytech4a.robocup.firebot.ui.GraphicViewPanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Pierre on 11/05/2015.
@@ -16,15 +21,16 @@ public class FileController {
         this.mainController = mainController;
 
         FilePanel filePanel = (FilePanel) mainController.getView().getFilePanel();
-        filePanel.getLoadGraphButton().addAction(new LoadGraphAction());
-        filePanel.getLoadImageButton().addAction(new LoadImageAction());
+        filePanel.getLoadGraphButton().addAction(new LoadGraphAction(mainController));
+        filePanel.getLoadImageButton().addAction(new LoadImageAction(mainController));
         filePanel.getSaveGraphButton().addAction(new SaveGraphAction());
     }
 }
 
 
 class LoadGraphAction extends AbstractAction{
-    public LoadGraphAction(){
+    MainController mainController;
+    public LoadGraphAction(MainController mainController){
     }
 
     @Override
@@ -34,19 +40,45 @@ class LoadGraphAction extends AbstractAction{
 
         if(fileChooser.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
             file = fileChooser.getSelectedFile();
+            //TODO charger le graphe
         }
     }
 }
 
 
 class LoadImageAction extends AbstractAction{
-    public LoadImageAction(){
-
+    MainController mainController;
+    public LoadImageAction(MainController mainController){
+        this.mainController = mainController;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        File file;
+        JFileChooser fileChooser = new JFileChooser(new File("."));
 
+        FileFilter type1 = new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                return f.getName().toLowerCase().endsWith(".jpeg") ||
+                        f.getName().toLowerCase().endsWith(".jpg") ||
+                        f.isDirectory();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Image extensions";
+            }
+        };
+        fileChooser.addChoosableFileFilter(type1);
+
+        if(fileChooser.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+            ImageIcon img = new ImageIcon(file.getAbsolutePath());
+            GraphicViewPanel graphicViewPanel = (GraphicViewPanel) mainController.getView().getGraphicViewPanel();
+            graphicViewPanel.setBasicImage(img);
+            graphicViewPanel.paintComponent(graphicViewPanel.getGraphics());
+        }
     }
 }
 
--- firebot/src/main/java/com/polytech4a/robocup/firebot/ui/GraphicViewPanel.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/ui/GraphicViewPanel.java
@@ -5,12 +5,15 @@ import com.polytech4a.robocup.firebot.ui.graphic.models.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Pierre on 06/05/2015.
  */
 public class GraphicViewPanel extends JPanel {
     private Graph graph;
+    private BufferedImage canvas;
+    private ImageIcon basicImage;
 
     public GraphicViewPanel(){
         super();
@@ -20,20 +23,38 @@ public class GraphicViewPanel extends JPanel {
         this.setBackground(Color.white);
         this.setDoubleBuffered(true);
         this.graph = new Graph();
+
+        canvas = new BufferedImage(700,400,BufferedImage.TYPE_INT_RGB);
     }
 
     public Graph getGraph() {
         return graph;
     }
 
+    public void setBasicImage(ImageIcon image) {
+        this.basicImage = image;
+    }
+
+    private void updateCanvas(){
+        Graphics2D g2d = canvas.createGraphics();
+        if(basicImage == null){
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setPaint(Color.white);
+            g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        }
+        else{
+            basicImage.paintIcon(null, g2d, 0, 0);
+            //g2d.drawImage(basicImage, 0, 0, 700, 400, null);
+        }
+
+        graph.drawGraph(g2d);
+        repaint();
+    }
+
     @Override
     public void paintComponent(Graphics g) {
-        g.setColor(Color.white);
-        Dimension dim = getSize();
-        g.fillRect(0, 0, dim.width, dim.height);
-        //super.paintComponent(g);
-
-        graph.drawGraph(g);
+        updateCanvas();
+        g.drawImage(canvas, 0, 0, null);
     }
 
     /**
@@ -45,7 +66,12 @@ public class GraphicViewPanel extends JPanel {
      * @param y2
      */
     public void paintComponentWithCustomEdge(Graphics g, int x1, int y1, int x2, int y2){
-        this.paintComponent(g);
-        g.drawLine(x1, y1, x2, y2);
+        updateCanvas();
+        Graphics2D g2d = (Graphics2D) canvas.getGraphics();
+        g2d.setPaint(Color.magenta);
+        g2d.drawLine(x1, y1, x2, y2);
+
+        repaint();
+        g.drawImage(canvas, 0, 0, null);
     }
 }
