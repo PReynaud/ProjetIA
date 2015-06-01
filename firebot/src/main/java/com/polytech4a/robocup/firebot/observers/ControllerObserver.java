585b279e73d750157c93d9cdcc47f5371a144f40
xenyztw <chaussende.adrien@gmail.com>
2015-06-01 15:31:00 +0200

    ACE : Add Observer/Observable.

---
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/observers/ControllerObserver.java
@@ -0,0 +1,21 @@
+package com.polytech4a.robocup.firebot.observers;
+
+import com.polytech4a.robocup.graph.enums.EdgeType;
+import com.polytech4a.robocup.graph.enums.NodeType;
+import com.polytech4a.robocup.graph.model.Edge;
+import com.polytech4a.robocup.graph.model.Node;
+
+/**
+ * Created by Adrien CHAUSSENDE on 01/06/2015.
+ *
+ * @author Adrien CHAUSSENDE
+ * @version 1.0
+ */
+public interface ControllerObserver {
+
+    public void updateEdgeType(Edge edge, EdgeType type);
+
+    public void updateNodeType(Node node, NodeType type);
+
+    public void updateRobotMovement(Node currentNode, Node nextNode, long time);
+}
---
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/observers/Observable.java
@@ -0,0 +1,54 @@
+package com.polytech4a.robocup.firebot.observers;
+
+import com.polytech4a.robocup.graph.enums.EdgeType;
+import com.polytech4a.robocup.graph.enums.NodeType;
+import com.polytech4a.robocup.graph.model.Edge;
+import com.polytech4a.robocup.graph.model.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Adrien CHAUSSENDE on 01/06/2015.
+ *
+ * @author Adrien CHAUSSENDE
+ * @version 1.0
+ */
+public class Observable {
+
+    private List<ControllerObserver> observers;
+
+    public Observable() {
+        this.observers = new LinkedList<>();
+    }
+
+    public void addControllerObserver(ControllerObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeControllerObserver(ControllerObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void removeControllerObservers() {
+        observers.clear();
+    }
+
+    public void fireUpdateEdgeType(Edge edge, EdgeType type) {
+        for(ControllerObserver observer : observers) {
+            observer.updateEdgeType(edge, type);
+        }
+    }
+
+    public void fireUpdateNodeType(Node node, NodeType type) {
+        for(ControllerObserver observer : observers) {
+            observer.updateNodeType(node, type);
+        }
+    }
+
+    public void fireUpdateRobotMovement(Node currentNode, Node nextNode, long time) {
+        for(ControllerObserver observer : observers) {
+            observer.updateRobotMovement(currentNode, nextNode, time);
+        }
+    }
+}
