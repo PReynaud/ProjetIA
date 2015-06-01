b6ca44b2ee95e047063f6c3df702ce3bd3dcabcd
Slashgear <antoine395.caron@laposte.net>
2015-06-01 14:12:11 +0200

    ACN: Refactor Main class and Add Log.

--- firebot/src/main/java/com/polytech4a/robocup/firebot/ui/Application.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/ui/Application.java
@@ -1,9 +1,9 @@
 package com.polytech4a.robocup.firebot.ui;
 
 import com.polytech4a.robocup.firebot.controller.MainController;
-import com.polytech4a.robocup.firebot.robots.Firebot;
 import com.polytech4a.robocup.firebot.robots.RobotManager;
 import com.polytech4a.robocup.graph.model.Graph;
+import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -11,13 +11,19 @@ import java.util.ArrayList;
 /**
  * Created by Pierre on 06/05/2015.
  */
-//TODO: classe à effacer, juste utile pour tester la fenêtre
-public class main {
+public class Application {
+
+    /**
+     * Logger.
+     */
+    private static final Logger logger = Logger.getLogger(Application.class);
+
     public static void main(String[] args){
+        logger.info("Starting Application...");
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 MainForm window = new MainForm();
-                RobotManager robotManager = new RobotManager(new ArrayList<Firebot>(), new Graph());
+                RobotManager robotManager = new RobotManager(new ArrayList<>(), new Graph());
                 MainController controller = new MainController(window, robotManager);
                 window.setVisible(true);
             }
