f78e89c4174a430e94f27cd24913c69a07398576
Slashgear <antoine395.caron@laposte.net>
2015-06-02 20:57:42 +0200

    ACN: MouseControlller Refactor for switch.

--- firebot/src/main/java/com/polytech4a/robocup/firebot/controller/MouseController.java
+++ firebot/src/main/java/com/polytech4a/robocup/firebot/controller/MouseController.java
@@ -1,6 +1,7 @@
 package com.polytech4a.robocup.firebot.controller;
 
 import com.polytech4a.robocup.firebot.robots.CrossCountryFirebot;
+import com.polytech4a.robocup.firebot.robots.Firebot;
 import com.polytech4a.robocup.firebot.robots.LeggedFirebot;
 import com.polytech4a.robocup.firebot.robots.TrackedFirebot;
 import com.polytech4a.robocup.firebot.ui.GraphicViewPanel;
@@ -18,6 +19,9 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.List;
 
+import static com.polytech4a.robocup.firebot.controller.EnumSelection.ADD_EDGE;
+import static com.polytech4a.robocup.firebot.controller.EnumSelection.ADD_STEEP_EDGE;
+
 /**
  * Created by Pierre on 16/05/2015.
  * Controller of the mouse and the different actions we can do with it
@@ -25,7 +29,7 @@ import java.util.List;
 public class MouseController implements MouseListener, MouseMotionListener {
     private MainController mainController;
 
-    public MouseController(MainController mainController){
+    public MouseController(MainController mainController) {
         this.mainController = mainController;
     }
 
@@ -35,78 +39,84 @@ public class MouseController implements MouseListener, MouseMotionListener {
         GraphView graph = graphicViewPanel.getGraph();
         Graph graphModel = this.mainController.getGraph();
 
-        /* Draw a node */
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_NODE) ||
-                mainController.getSelectionMode().equals(EnumSelection.ADD_FIRE_NODE)){
-            if(mainController.getSelectionMode().equals(EnumSelection.ADD_NODE)){
+        switch (mainController.getSelectionMode()) {
+            case ADD_NODE:
                 int id = mainController.getGraph().addNewNode(e.getX(), e.getY(), NodeType.NORMAL);
                 graph.addNode(e.getX(), e.getY(), id);
-            }
-            if(mainController.getSelectionMode().equals(EnumSelection.ADD_FIRE_NODE)){
+                break;
+            case ADD_FIRE_NODE:
                 NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
-                if(clickedNode != null){
+                if (clickedNode != null) {
                     //TODO ajouter au modèle
-                    int id = clickedNode.getId();
+                    id = clickedNode.getId();
                     graph.deleteNode(clickedNode.getId());
                     graph.addFireNode(clickedNode.getX(), clickedNode.getY(), id);
                 }
-            }
-        }
-
-        /* Draw an edge */
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_EDGE) ||
-                mainController.getSelectionMode().equals(EnumSelection.ADD_STEEP_EDGE)){
-            //We verify that we click on a node
-            NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
-            if(clickedNode != null){
-                if(mainController.getLastClickedNode() == null){
-                    mainController.setLastClickedNode(clickedNode);
-                }
-                else{
-                    if(mainController.getSelectionMode().equals(EnumSelection.ADD_EDGE)){
+                break;
+            case ADD_EDGE:
+                clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
+                if (clickedNode != null) {
+                    if (mainController.getLastClickedNode() == null) {
+                        mainController.setLastClickedNode(clickedNode);
+                    } else {
                         mainController.getGraph().addEdge(new Edge(mainController.getLastClickedNode().getId(), clickedNode.getId(), EdgeType.PLAT));
                         graph.addEdge(mainController.getLastClickedNode(), clickedNode);
+                        mainController.setLastClickedNode(null);
                     }
-                    if(mainController.getSelectionMode().equals(EnumSelection.ADD_STEEP_EDGE)){
+                }
+                break;
+            case ADD_STEEP_EDGE:
+                clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
+                if (clickedNode != null) {
+                    if (mainController.getLastClickedNode() == null) {
+                        mainController.setLastClickedNode(clickedNode);
+                    } else {
                         mainController.getGraph().addEdge(new Edge(mainController.getLastClickedNode().getId(), clickedNode.getId(), EdgeType.ESCARPE));
                         graph.addSteepEdge(mainController.getLastClickedNode(), clickedNode);
+                        mainController.setLastClickedNode(null);
                     }
-                    mainController.setLastClickedNode(null);
                 }
-            }
+                break;
+            case ADD_CROSS_COUNTRY_FIREBOT:
+                Firebot bot = new CrossCountryFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
+                NodeView nView = addFireBot(bot, graph, e);
+                if (nView != null) {
+                    graph.addCrossCountryFirebot(nView, bot.getId());
+                }
+                break;
+            case ADD_LEGGED_FIREBOT:
+                bot = new LeggedFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
+                nView = addFireBot(bot, graph, e);
+                if (nView != null) {
+                    graph.addLeggedFirebot(nView, bot.getId());
+                }
+                break;
+            case ADD_TRACKED_FIREBOT:
+                bot = new TrackedFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
+                nView = addFireBot(bot, graph, e);
+                if (nView != null) {
+                    graph.addTrackedFirebot(nView, bot.getId());
+                }
+                break;
+            default:
+                break;
         }
+    }
 
-        /* Draw a robot */
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_CROSS_COUNTRY_FIREBOT)){
-            NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
-
-            if(clickedNode != null){
-                Node clickedNodeModel = mainController.getModel().getGraph().getNode(clickedNode.getId());
-                CrossCountryFirebot newBot = new CrossCountryFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
-                newBot.setCurrentNode(clickedNodeModel);
-                newBot.addControllerObserver(mainController);
-                this.mainController.getModel().getRobotTeam().add(newBot);
-                graph.addCrossCountryFirebot(clickedNode, newBot.getId());
-            }
+    /**
+     * Add a Firebot to the mainController.
+     *
+     * @param firebot firebot of the specific type.
+     */
+    private NodeView addFireBot(Firebot firebot, GraphView graph, MouseEvent e) {
+        NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
+        if (clickedNode != null) {
+            Node clickedNodeModel = mainController.getModel().getGraph().getNode(clickedNode.getId());
+            firebot.setCurrentNode(clickedNodeModel);
+            firebot.addControllerObserver(mainController);
+            this.mainController.getModel().getRobotTeam().add(firebot);
         }
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_LEGGED_FIREBOT)){
-            NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
-            if(clickedNode != null){
-                LeggedFirebot newBot = new LeggedFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
-                newBot.addControllerObserver(mainController);
-                this.mainController.getModel().getRobotTeam().add(newBot);
-                graph.addLeggedFirebot(clickedNode, newBot.getId());
-            }
-        }
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_TRACKED_FIREBOT)){
-            NodeView clickedNode = clickOnANode(graph.getNodes(), e.getX(), e.getY());
-            if(clickedNode != null){
-                TrackedFirebot newBot = new TrackedFirebot(mainController.getModel().generateId(), graphModel, 100, new AStar());
-                newBot.addControllerObserver(mainController);
-                this.mainController.getModel().getRobotTeam().add(newBot);
-                graph.addTrackedFirebot(clickedNode, newBot.getId());
-            }
-        }
+        return clickedNode;
     }
 
     @Override
@@ -136,8 +146,8 @@ public class MouseController implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        if(mainController.getSelectionMode().equals(EnumSelection.ADD_EDGE) ||
-                mainController.getSelectionMode().equals(EnumSelection.ADD_STEEP_EDGE)) {
+        if (mainController.getSelectionMode().equals(ADD_EDGE) ||
+                mainController.getSelectionMode().equals(ADD_STEEP_EDGE)) {
             if (mainController.getLastClickedNode() != null) {
                 GraphicViewPanel graphicViewPanel = (GraphicViewPanel) mainController.getView().getGraphicViewPanel();
                 graphicViewPanel.paintComponentWithCustomEdge(graphicViewPanel.getGraphics(), mainController.getLastClickedNode().getX(),
@@ -146,10 +156,10 @@ public class MouseController implements MouseListener, MouseMotionListener {
         }
     }
 
-    private NodeView clickOnANode(List<NodeView> listOfNodes, int x, int y){
-        for(NodeView node: listOfNodes){
+    private NodeView clickOnANode(List<NodeView> listOfNodes, int x, int y) {
+        for (NodeView node : listOfNodes) {
             double dist = Math.sqrt(Math.pow((node.getX() - x), 2) + Math.pow((node.getY() - y), 2));
-            if(dist < node.getSize()){
+            if (dist < node.getSize()) {
                 return node;
             }
         }
